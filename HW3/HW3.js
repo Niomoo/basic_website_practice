7883e4e8894e6373c29820ca6de381cb470e7329
Niomoo <jennyliu.lyh@gmail.com>
2021-07-12 17:49:30 +0800

    [javascript]Modify variable type and make code prettier

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -3,8 +3,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
@@ -150,30 +150,30 @@ EvilCircle.prototype.collisionDetect = function() {
   }
 }
 
-var evilCircle = new EvilCircle(random(20,width - 20),random(20,height - 20));
+let evilCircle = new EvilCircle(random(20, width - 20), random(20, height - 20));
 evilCircle.setControls();
 
-var counter = document.getElementById('count');
-var ballsCount = 0;
+let counter = document.getElementById('count');
+let ballsCount = 0;
 
 // define loop that keeps drawing the scene constantly
 
 function loop() {
 	
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
 
 	var n = 0;
 	while(balls.length < 25) {
-		const size = random(10,20);
+		const size = random(10, 20);
 		let ball = new Ball(
 			// ball position always drawn at least one ball width
 			// away from the edge of the canvas, to avoid drawing errors
 			random(0 + size,width - size),
 			random(0 + size,height - size),
-			random(-7,7),
-			random(-7,7),
-			'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+			random(-7, 7),
+			random(-7, 7),
+			'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')',
 			size,
 			true,
 		);
